37b3042c578d640fd80c339d76de2f494778c5d9
Tilly-gee <104358587+Tilly-gee@users.noreply.github.com>
2023-07-21 10:03:40 +0100

    Update vercel.sh

--- vercel.sh
+++ vercel.sh
@@ -4,7 +4,7 @@ echo "VERCEL_ENV: $VERCEL_ENV"
 echo "HUGO_VERSION: $HUGO_VERSION"
 
 if [[ $VERCEL_ENV == "production"  ]] ; then 
-  amazon-linux-extras install golang1.11 && hugo -e="production" --gc --minify --ignoreCache --verbose
+  amazon-linux-extras install golang1.11 && hugo --gc --minify --ignoreCache --verbose
 else 
   amazon-linux-extras install golang1.11 && hugo -e="staging" --gc --minify --ignoreCache --verbose
 fi
